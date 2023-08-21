c813e510f4ee728c7d1710922b4fb6c240452075
Abigail Miller <millerabigail266@gmail.com>
2023-08-21 17:46:09 -0500

    UPDATED: Changes in week2 and week3
    project week (6a5780ea2f0430a9b3617563faab200137f650c3 -> 3c0e5d12a01fd36a269188adfaae82f17da2620f)
    - Updated subproject commit for week3 (092bd897f73693d0349fb4a9d02e0010e)

--- week1/DAY-2-SOLO copy.js
+++ week1/DAY-2-SOLO copy.js
@@ -29,33 +29,33 @@ console.log(twoSum([4, 6, 2, 3], 11)); // false
 The function counts both lowercase (e) and uppercase (E). use a while loop instead*/
 
 function eCounter(word) {
-    let count = 0;
+  let count = 0;
 
-    for (let index = 0; index < word.length; index++) {
-      let char = word[index];
-      if (char === "e" || char === "E") {
-        count += 1;
-      }
+  for (let index = 0; index < word.length; index++) {
+    let char = word[index];
+    if (char === "e" || char === "E") {
+      count += 1;
     }
+  }
 
-    return count;
-  };
+  return count;
+}
 
 function eCounter(word) {
   let count = 0;
   let i = 0;
-  while (i < word.length){
+  while (i < word.length) {
     i++;
     let char = word[i];
-    if (char === "e" || char === "E"){
-    count +=1;
-      }
+    if (char === "e" || char === "E") {
+      count += 1;
+    }
   }
-    return count;
+  return count;
 }
-  console.log(eCounter("apple")); // => 1
-  console.log(eCounter("appleapple")); // => 2
-  console.log(eCounter("Appleee")); // => 3
+console.log(eCounter("apple")); // => 1
+console.log(eCounter("appleapple")); // => 2
+console.log(eCounter("Appleee")); // => 3
 
 /*|||||||||||||||||||||||||||||||||||||||||||||||||||
 //Define a function isPrime(number) that returns true if number is prime.
--- week1/DAY-2-SOLO.js
+++ week1/DAY-2-SOLO.js
@@ -7,13 +7,13 @@
 /* Write a function twoSum(arr, target) that accepts an array and a target number as args. The function should a return a boolean indicating if two
 distinct numbers of the array add up to the target value. You can assume that input array contains only unique numbers. */
 
-function twoSum(arr, target){
-for(let i = 0; i < arr.length; i++){
-  for (let j = i + 1; j < arr.length; j++){
-    if(arr[i] + arr[j] === target){
-      return true;
-//The function should return a boolean if two num in arr..
-// nums1 + num2 = target
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return true;
+        //The function should return a boolean if two num in arr..
+        // nums1 + num2 = target
       }
     }
   }
