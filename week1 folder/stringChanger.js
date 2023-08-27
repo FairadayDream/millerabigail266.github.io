c082c5bf20fdd562cd59b23341bbab96378bb767
Your Name <youremail@example.com>
2023-08-27 15:05:07 -0500

    adsfg

--- week1 folder/stringChanger.js
+++ week1 folder/stringChanger.js
@@ -20,8 +20,7 @@ console.log(stringChanger("foo", "capitalize")); //"Foo"
 console.log(stringChanger("foo", "uppercase")); //FOO
 console.log(stringChanger("foo", "double")); //"foofoo"
 console.log(stringChanger("foo", "reverse")); //"oof"
-console.log(stringChanger("foo", "reverse")); //"oof"
-console.log(stringChanger("foo", "reverse")); //"oof"
+
 // TESTS
 // DO NOT MODIFY ANYTHING BELOW THIS LINE
 
